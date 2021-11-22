13cdac7f8bd91cae30506ac500189312003a1dad
Lucas <lucastl1@al.insper.edu.br>
2021-11-22 17:38:58 -0300

    estrutura

--- Versão1.py
+++ Versão1.py
@@ -1,22 +1,86 @@
 import pygame
 import random
 
-
 pygame.init()
 
 # -- Tela inicial
-largura = 600
+largura = 1200
 altura = 800
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Space run')
 
+
+# -- inicio do jogo
+
+largura_aviao = 200
+altura_aviao = 100
+largura_nave = 50
+altura_nave = 250
+font = pygame.font.SysFont(None, 48)
+
+background = pygame.image.load('imagem1.png').convert()
+background = pygame.transform.scale(background,(largura,altura))
+
+image_aviao = pygame.image.load('aviao.png').convert()
+image_aviao = pygame.transform.scale(image_aviao,(largura_aviao,altura_aviao))
+
+image_nave = pygame.image.load('Shuttle.png').convert()
+image_nave = pygame.transform.scale(image_nave,(largura_nave,altura_nave))
+
+# -- estrutura dos dados
+
+class Nave(pygame.sprite.Sprite):
+    def __init__(self,img):
+        pygame.sprite.Sprite._init_(self)
+        
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = largura / 2
+        self.rect.bottom = altura - 10
+        self.speedx = 0
+        
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+
+        # Mantem dentro da tela
+        if self.rect.right > largura:
+            self.rect.right = largura
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+class aviao(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, largura-largura_aviao)
+        self.rect.y = random.randint(-100, -altura_aviao)
+        self.speedx = random.randint(-3, 3)
+        self.speedy = random.randint(2, 9)
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > altura or self.rect.right < 0 or self.rect.left > largura:
+            self.rect.x = random.randint(0, largura-largura,)
+            self.rect.y = random.randint(-100, -altura_aviao)
+            self.speedx = random.randint(-3, 3)
+            self.speedy = random.randint(2, 9)
+
+        
+        
+        
+        
 # -- Estrutura inicial do jogo
 
-game = True
-
-# -- Imagem de inicio
-image = pygame.image.load('Dessoft-PF/imagem1.png').convert()
-
+game = True        
+    
 # -- Loop principal
 
 while game:
@@ -28,7 +92,7 @@ while game:
 
     # ----- Gera saídas
     tela.fill((0,0,0))  # Preenche com a cor branca
-    tela.blit(image, (10, 10))
+    tela.blit(background, (10, 10))
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
